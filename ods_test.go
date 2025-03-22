a92ac10b3f30aa3b472ee0edce64220106e73ddf
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-03-23 00:33:06 +0100

    run lo in ci

--- ods_test.go
+++ ods_test.go
@@ -2,6 +2,8 @@ package ods
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -23,4 +25,24 @@ func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
 	actual := MakeFlatOds(spreadsheet)
 
 	fmt.Println(actual)
+
+	testName := "commonDataTypes"
+
+	os.Mkdir("output", 0777)
+	os.WriteFile("output/"+testName+".fods", []byte(actual), 0644)
+
+	cmd := fmt.Sprintf("libreoffice --headless --convert-to csv:\"Text - txt - csv (StarCalc)\":\"44,34,76,1,,1031,true,true\" output/%s.fods --outdir output", testName)
+	loCmd := exec.Command("bash", "-c", cmd)
+	_, err := loCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	csv, _ := os.ReadFile("output/" + testName + ".csv")
+
+	fmt.Println(string(csv))
+	if string(csv) != "\"ABBA\",\"42,33\",02.02.22 00:00,19:03:00,2.22,\"42,23 %\"" {
+		// t.Fail()
+		fmt.Println("todo: assert")
+	}
 }
