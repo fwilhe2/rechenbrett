3a51c20b16872cb5bdfb83efe7fba3535c022d40
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-03-23 11:59:21 +0100

    compare csv elements in test

--- ods_test.go
+++ ods_test.go
@@ -1,9 +1,13 @@
 package ods
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 )
 
@@ -30,7 +34,8 @@ func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
 	testName := "commonDataTypes"
 
 	os.Mkdir("output", 0777)
-	os.WriteFile("output/"+testName+".fods", []byte(actual), 0644)
+
+	os.WriteFile(fmt.Sprintf("output/%s.fods", testName), []byte(actual), 0644)
 
 	cmd := fmt.Sprintf("libreoffice --headless --convert-to csv:\"Text - txt - csv (StarCalc)\":\"44,34,76,1,,1031,true,true\" output/%s.fods --outdir output", testName)
 	loCmd := exec.Command("bash", "-c", cmd)
@@ -39,11 +44,32 @@ func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
 		panic(err)
 	}
 
-	csv, _ := os.ReadFile("output/" + testName + ".csv")
+	actualCsv, _ := os.ReadFile(fmt.Sprintf("output/%s.csv", testName))
 
-	fmt.Println(string(csv))
-	if string(csv) != "\"ABBA\",\"42,33\",02.02.22 00:00,19:03:00,2.22,\"42,23 %\"" {
-		// t.Fail()
-		fmt.Println("todo: assert")
+	fmt.Println(string(actualCsv))
+
+	r := csv.NewReader(strings.NewReader(string(actualCsv)))
+
+	expected := []string{"ABBA", "42,33", "2022-02-02", "19:03:00", "2.22€", "-2.22€", "42,23 %"}
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+
+		for i, v := range record {
+			if (v != expected[i]) {
+				fmt.Printf("Failed compare, is: %s, expected: %s", v, expected[i])
+				t.Fail()
+			}
+		}
 	}
+
+
 }
