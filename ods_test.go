9852b23acd74a1a87f6df334e57af7dd4de0195a
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-03-23 19:29:51 +0100

    refactor test

--- ods_test.go
+++ ods_test.go
@@ -11,28 +11,10 @@ import (
 	"testing"
 )
 
-func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
-
-	cells := [][]Cell{
-		{
-			MakeCell("ABBA", "string"),
-			MakeCell("42.3324", "float"),
-			MakeCell("2022-02-02", "date"),
-			MakeCell("19:03:00", "time"),
-			MakeCell("2.22", "currency"),
-			MakeCell("-2.22", "currency"),
-			MakeCell("0.4223", "percentage"),
-		},
-	}
-
-	spreadsheet := MakeSpreadsheet(cells)
+func integrationTest(testName string, inputCells [][]Cell, expectedCsv [][]string) error {
+	spreadsheet := MakeSpreadsheet(inputCells)
 
 	actual := MakeFlatOds(spreadsheet)
-
-	fmt.Println(actual)
-
-	testName := "commonDataTypes"
-
 	os.Mkdir("output", 0777)
 
 	os.WriteFile(fmt.Sprintf("output/%s.fods", testName), []byte(actual), 0644)
@@ -44,14 +26,13 @@ func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
 		panic(err)
 	}
 
-	actualCsv, _ := os.ReadFile(fmt.Sprintf("output/%s.csv", testName))
+	actualCsvBytes, _ := os.ReadFile(fmt.Sprintf("output/%s.csv", testName))
 
-	fmt.Println(string(actualCsv))
+	actualCsv := string(actualCsvBytes)
 
-	r := csv.NewReader(strings.NewReader(string(actualCsv)))
+	r := csv.NewReader(strings.NewReader(actualCsv))
 
-	expected := []string{"ABBA", "42,33", "2022-02-02", "19:03:00", "2.22€", "-2.22€", "42,23 %"}
-
+	line := 0
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -64,12 +45,43 @@ func TestMakeSpreadsheetWithCommonDataTypes(t *testing.T) {
 		fmt.Println(record)
 
 		for i, v := range record {
-			if (v != expected[i]) {
-				fmt.Printf("Failed compare, is: %s, expected: %s", v, expected[i])
-				t.Fail()
+			if v != expectedCsv[line][i] {
+				return fmt.Errorf("[%s] Failed test, value is: '%s', expected: '%s'", testName, v, expectedCsv[line][i])
 			}
 		}
 	}
+	return nil
+}
 
+func TestCommonDataTypes(t *testing.T) {
+	givenThoseCells := [][]Cell{
+		{
+			MakeCell("ABBA", "string"),
+			MakeCell("42.3324", "float"),
+			MakeCell("2022-02-02", "date"),
+			MakeCell("19:03:00", "time"),
+			MakeCell("2.22", "currency"),
+			MakeCell("-2.22", "currency"),
+			MakeCell("0.4223", "percentage"),
+		},
+	}
 
+	expectedThisCsv := [][]string{
+		{
+			"ABBA",
+			"42,33",
+			"2022-02-02",
+			"19:03:00",
+			"2.22€",
+			"-2.22€",
+			"42,23 %",
+		},
+	}
+
+	err := integrationTest("common-data-types", givenThoseCells, expectedThisCsv)
+
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		t.Fail()
+	}
 }
