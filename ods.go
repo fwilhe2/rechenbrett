20eda056d8e1cb5591a43c48d6c370eb950de31f
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-03-23 20:01:25 +0100

    Minimize xml namespaces

--- ods.go
+++ ods.go
@@ -34,44 +34,15 @@ func MakeSpreadsheet(cells [][]Cell) Spreadsheet {
 func MakeFlatOds(spreadsheet Spreadsheet) string {
 
 	fods := FlatOds{
-		XMLNSOffice:       "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
-		XMLNSTable:        "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
-		XMLNSText:         "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
-		XMLNSStyle:        "urn:oasis:names:tc:opendocument:xmlns:style:1.0",
-		XMLNSFo:           "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0",
-		XMLNSSvg:          "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0",
-		XMLNSChart:        "urn:oasis:names:tc:opendocument:xmlns:chart:1.0",
-		XMLNSDr3d:         "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0",
-		XMLNSMath:         "http://www.w3.org/1998/Math/MathML",
-		XMLNSForm:         "urn:oasis:names:tc:opendocument:xmlns:form:1.0",
-		XMLNSScript:       "urn:oasis:names:tc:opendocument:xmlns:script:1.0",
-		XMLNSConfig:       "urn:oasis:names:tc:opendocument:xmlns:config:1.0",
-		XMLNSXlink:        "http://www.w3.org/1999/xlink",
-		XMLNSDc:           "http://purl.org/dc/elements/1.1/",
-		XMLNSMeta:         "urn:oasis:names:tc:opendocument:xmlns:meta:1.0",
-		XMLNSNumber:       "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0",
-		XMLNSOf:           "urn:oasis:names:tc:opendocument:xmlns:of:1.2",
-		XMLNSXforms:       "http://www.w3.org/2002/xforms",
-		XMLNSXsd:          "http://www.w3.org/2001/XMLSchema",
-		XMLNSXsi:          "http://www.w3.org/2001/XMLSchema-instance",
-		XMLNSGrddl:        "http://www.w3.org/2003/g/data-view#",
-		XMLNSXhtml:        "http://www.w3.org/1999/xhtml",
-		XMLNSPresentation: "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0",
-		XMLNSCss3t:        "http://www.w3.org/TR/css3-text/",
-		XMLNSFormx:        "urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0",
-		XMLNSOooc:         "http://openoffice.org/2004/calc",
-		XMLNSOoow:         "http://openoffice.org/2004/writer",
-		XMLNSRpt:          "http://openoffice.org/2005/report",
-		XMLNSDraw:         "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0",
-		XMLNSOoo:          "http://openoffice.org/2004/office",
-		XMLNSCalcext:      "urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0",
-		XMLNSTableooo:     "http://openoffice.org/2009/table",
-		XMLNSDrawooo:      "http://openoffice.org/2010/draw",
-		XMLNSLoext:        "urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0",
-		XMLNSDom:          "http://www.w3.org/2001/xml-events",
-		XMLNSField:        "urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0",
-		OfficeVersion:     "1.3",
-		OfficeMimetype:    "application/vnd.oasis.opendocument.spreadsheet",
+		XMLNSOffice:    "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+		XMLNSTable:     "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
+		XMLNSText:      "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
+		XMLNSStyle:     "urn:oasis:names:tc:opendocument:xmlns:style:1.0",
+		XMLNSFo:        "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0",
+		XMLNSNumber:    "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0",
+		XMLNSCalcext:   "urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0",
+		OfficeVersion:  "1.3",
+		OfficeMimetype: "application/vnd.oasis.opendocument.spreadsheet",
 		AutomaticStyles: AutomaticStyles{
 			NumberStyles: createNumberStyles(),
 			Styles:       createStyles(),
@@ -243,47 +214,18 @@ type Table struct {
 }
 
 type FlatOds struct {
-	XMLName           xml.Name        `xml:"office:document"`
-	XMLNSOffice       string          `xml:"xmlns:office,attr"`
-	XMLNSTable        string          `xml:"xmlns:table,attr"`
-	XMLNSText         string          `xml:"xmlns:text,attr"`
-	XMLNSStyle        string          `xml:"xmlns:style,attr"`
-	XMLNSFo           string          `xml:"xmlns:fo,attr"`
-	XMLNSSvg          string          `xml:"xmlns:svg,attr"`
-	XMLNSChart        string          `xml:"xmlns:chart,attr"`
-	XMLNSDr3d         string          `xml:"xmlns:dr3d,attr"`
-	XMLNSMath         string          `xml:"xmlns:math,attr"`
-	XMLNSForm         string          `xml:"xmlns:form,attr"`
-	XMLNSScript       string          `xml:"xmlns:script,attr"`
-	XMLNSConfig       string          `xml:"xmlns:config,attr"`
-	XMLNSXlink        string          `xml:"xmlns:xlink,attr"`
-	XMLNSDc           string          `xml:"xmlns:dc,attr"`
-	XMLNSMeta         string          `xml:"xmlns:meta,attr"`
-	XMLNSNumber       string          `xml:"xmlns:number,attr"`
-	XMLNSOf           string          `xml:"xmlns:of,attr"`
-	XMLNSXforms       string          `xml:"xmlns:xforms,attr"`
-	XMLNSXsd          string          `xml:"xmlns:xsd,attr"`
-	XMLNSXsi          string          `xml:"xmlns:xsi,attr"`
-	XMLNSGrddl        string          `xml:"xmlns:grddl,attr"`
-	XMLNSXhtml        string          `xml:"xmlns:xhtml,attr"`
-	XMLNSPresentation string          `xml:"xmlns:presentation,attr"`
-	XMLNSCss3t        string          `xml:"xmlns:css3t,attr"`
-	XMLNSFormx        string          `xml:"xmlns:formx,attr"`
-	XMLNSOooc         string          `xml:"xmlns:oooc,attr"`
-	XMLNSOoow         string          `xml:"xmlns:ooow,attr"`
-	XMLNSRpt          string          `xml:"xmlns:rpt,attr"`
-	XMLNSDraw         string          `xml:"xmlns:draw,attr"`
-	XMLNSOoo          string          `xml:"xmlns:ooo,attr"`
-	XMLNSCalcext      string          `xml:"xmlns:calcext,attr"`
-	XMLNSTableooo     string          `xml:"xmlns:tableooo,attr"`
-	XMLNSDrawooo      string          `xml:"xmlns:drawooo,attr"`
-	XMLNSLoext        string          `xml:"xmlns:loext,attr"`
-	XMLNSDom          string          `xml:"xmlns:dom,attr"`
-	XMLNSField        string          `xml:"xmlns:field,attr"`
-	OfficeVersion     string          `xml:"office:version,attr"`
-	OfficeMimetype    string          `xml:"office:mimetype,attr"`
-	AutomaticStyles   AutomaticStyles `xml:"office:automatic-styles"`
-	Body              Body            `xml:"office:body"`
+	XMLName         xml.Name        `xml:"office:document"`
+	XMLNSOffice     string          `xml:"xmlns:office,attr"`
+	XMLNSTable      string          `xml:"xmlns:table,attr"`
+	XMLNSText       string          `xml:"xmlns:text,attr"`
+	XMLNSStyle      string          `xml:"xmlns:style,attr"`
+	XMLNSFo         string          `xml:"xmlns:fo,attr"`
+	XMLNSNumber     string          `xml:"xmlns:number,attr"`
+	XMLNSCalcext    string          `xml:"xmlns:calcext,attr"`
+	OfficeVersion   string          `xml:"office:version,attr"`
+	OfficeMimetype  string          `xml:"office:mimetype,attr"`
+	AutomaticStyles AutomaticStyles `xml:"office:automatic-styles"`
+	Body            Body            `xml:"office:body"`
 }
 
 type Styles struct {
